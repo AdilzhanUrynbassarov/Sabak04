81fb41ed906f9bc878ad2f5729ff810b4c118eac
Адильжан Урынбасаров <adik@MacBook-Air-Adilzan.local>
2021-02-15 11:41:02 +0600

    practice 04 is done

---
+++ exp01/routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/myName', function () {
+    return view('name');
+});
+Route::get('/myName/{id?}/{phone?}', function ($id="", $phone="") {
+    return "Phone number is ".$phone." and id is ".$id;
+});
+
